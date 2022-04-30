12131210d7230ad87e735a7768bcd4c09f18cbfe
Michał Kupczak <kurczak12333@gmail.com>
2022-04-30 22:01:42 +0200

    Implemented templates for every page

--- App/src/components/common/CommonPageMenu/CommonPageMenu.tsx
+++ App/src/components/common/CommonPageMenu/CommonPageMenu.tsx
@@ -1,16 +1,25 @@
 import styles from "./CommonPageMenu.module.scss";
-
+import LogOutIcon from "public/LogOutIcon.svg";
+import CommonPageMenuLogic from "./CommonPageMenuLogic";
 interface ICommonPageMenu {
   menuContent: JSX.Element;
   content: JSX.Element;
 }
 
 const CommonPageMenu = ({ menuContent, content }: ICommonPageMenu) => {
+  const { handleLogout } = CommonPageMenuLogic();
   return (
     <div className={styles.siteLayout}>
       <div className={styles.menu}>
-        <div>{menuContent}</div>
-        <div>Log out</div>
+        <div className={styles.menuWrapper}>
+          {menuContent}
+          <div className={styles.menuBottom} onClick={handleLogout}>
+            <div className={styles.logOutButton}>
+              <img src={LogOutIcon} alt={"Log out icon"} />
+            </div>
+            <span className={styles.logOutButtonText}>Log out</span>
+          </div>
+        </div>
       </div>
       <div className={styles.contentLayout}>{content}</div>
     </div>
