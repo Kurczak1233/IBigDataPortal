73dc6dc19d52766797099d5122edf9d1f9002eec
Michał Kupczak <kurczak12333@gmail.com>
2022-05-14 18:17:19 +0200

    Fixed warnings

--- App/src/redux/store.tsx
+++ App/src/redux/store.tsx
@@ -11,7 +11,7 @@ import {
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-const reducers = combineReducers({}); 
+const reducers = combineReducers({});
 
 const persistConfig = {
   key: "root",
