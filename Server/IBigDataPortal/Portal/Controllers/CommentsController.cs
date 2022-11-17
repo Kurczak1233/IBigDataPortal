4295550a68f5213d06f8c8ba5cc07992c598afa2
Michal Kupczak <kurczak12333@gmail.com>
2022-11-17 20:44:55 +0100

    Fixed styling of endpoints

--- Server/IBigDataPortal/Portal/Controllers/CommentsController.cs
+++ Server/IBigDataPortal/Portal/Controllers/CommentsController.cs
@@ -11,39 +11,42 @@ namespace IBigDataPortal.Controllers;
 [ApiController]
 [Authorize]
 [Route("[controller]")]
-public class CommentsController  : ControllerBase
+public class CommentsController : ControllerBase
 {
     private readonly IMediator _mediator;
     private readonly IUser _user;
     private readonly IAuthorizationService _authorizationService;
-    
+
     public CommentsController(IMediator mediator, IUser user, IAuthorizationService authorizationService)
     {
         _mediator = mediator;
         _user = user;
         _authorizationService = authorizationService;
     }
-    
+
     [HttpPost]
     public async Task<ActionResult> CreateComment(CreateCommentRequest body)
     {
-        await _authorizationService.AuthorizeAsync(_user.UserClaims, body, new CreateCommentsAuthorizationRequirement(body.ArticleId, _user.Id, body.ArticleType));
+        await _authorizationService.AuthorizeAsync(_user.UserClaims, body,
+            new CreateCommentsAuthorizationRequirement(body.ArticleId, _user.Id, body.ArticleType));
         var commentId = await _mediator.Send(new CreateCommentCommand(_user.Id, body));
         return Ok(commentId);
     }
-    
+
     [HttpPut]
     public async Task<ActionResult> UpdateComment(UpdateCommentRequest body)
     {
-        await _authorizationService.AuthorizeAsync(_user.UserClaims, body, new CreateCommentsAuthorizationRequirement(body.ArticleId, _user.Id, body.ArticleType));
+        await _authorizationService.AuthorizeAsync(_user.UserClaims, body,
+            new EditCommentsAuthorizationRequirement(body.ArticleId, _user.Id, body.ArticleType, body.ArticleId));
         await _mediator.Send(new UpdateCommentCommand(body));
         return Ok();
     }
-    
+
     [HttpPut("Delete")]
     public async Task<ActionResult> DeleteComment(DeleteCommentRequest body)
     {
-        await _authorizationService.AuthorizeAsync(_user.UserClaims, body, new CreateCommentsAuthorizationRequirement(body.ArticleId, _user.Id, body.ArticleType));
+        await _authorizationService.AuthorizeAsync(_user.UserClaims, body,
+            new EditCommentsAuthorizationRequirement(body.ArticleId, _user.Id, body.ArticleType, body.ArticleId));
         await _mediator.Send(new DeleteCommentCommand(body.CommentId));
         return Ok();
     }
